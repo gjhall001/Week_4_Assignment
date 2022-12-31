20cd1656aa527c49c22c71c6d60e61548198688b
gjhall001 <109775481+gjhall001@users.noreply.github.com>
2022-12-31 02:42:49 -0600

    main branch first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+function details(name, height, country) {
+    alert("My name is " + name + ". I am " + height + " tall and I was born in the " + country + ".");
+}
+
+details("Gregory Hall", "6'0", "United States of America");
